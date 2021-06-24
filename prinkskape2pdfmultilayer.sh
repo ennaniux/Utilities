b9978ac389d939805a2ac28194cad8a8582f963a
Daniel B <danielyho@yahoo.com>
2021-06-24 15:05:17 +0200

    change name update prinkscape

---
+++ prinkskape2pdfmultilayer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# print inkscape to pdf multiple layers
+# www.github.com/ennaniux/Utilities
+
+FILE=$1
+
+
+
+UL=$(inkscape --query-all $FILE | grep layer | awk -F"," '{print $1}')
+
+echo $UL
+
+for id in $UL; do echo "$FILE -jC -i $id -e $id.pdf"; done | inkscape --shell
+
+pdfjam layer*.pdf -o "$FILE_pout.pdf"
+
+rm layer*.pdf
+
